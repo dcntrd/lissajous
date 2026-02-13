97f82d4c98b2b95a9f25809cd44d76071c54dd01
fm <dev@decentered.net>
2026-02-13 23:45:54 +0100

    Initial commit

---
+++ src/main/java/net/decentered/stuff/Main.java
@@ -0,0 +1,15 @@
+package net.decentered.stuff;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            frame.setVisible(true);
+        });
+
+    }
+}
---
+++ src/main/java/net/decentered/stuff/MainFrame.java
@@ -0,0 +1,16 @@
+package net.decentered.stuff;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+
+        setTitle("Lissajous");
+        setLayout(new BorderLayout());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        
+    }
+}
